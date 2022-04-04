eb241dfd81de24522fce3b9e67f1215097260e84
Dylan Jordan <kyle.dylan.jordan@gmail.com>
2022-04-04 15:30:35 -0400

    commit tree 046694711f8fb704bd99d2613824d1c04f5ebcb9

--- index.js
+++ index.js
@@ -1,1 +1,38 @@
 // Code your solution in this file!
+function distanceFromHqInBlocks(streetNumber)
+{
+    return Math.abs(42 - streetNumber);
+}
+
+function distanceFromHqInFeet(streetNumber)
+{
+    return 264 * distanceFromHqInBlocks(streetNumber);
+}
+
+function distanceTravelledInFeet(street1, street2)
+{
+    return 264 * Math.abs(street1 - street2);
+}
+
+function calculatesFarePrice(start, destination)
+{
+    let distance = distanceTravelledInFeet(start, destination);
+
+    if (distance <= 400)
+    {
+        return 0;
+    }
+    else if (distance <= 2000)
+    {
+        distance -= 400;
+        return distance * .02;
+    }
+    else if (distance <= 2500)
+    {
+        return 25;
+    }
+    else
+    {
+        return 'cannot travel that far';
+    }
+}
